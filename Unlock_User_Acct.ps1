fbfeaac047e5300bfcf240266a573acc969296e5
Amanda Polak <99432906+amandap3@users.noreply.github.com>
2022-03-16 13:31:29 -0500

    Add files via upload

---
+++ Unlock_User_Acct.ps1
@@ -0,0 +1,9 @@
+Impport-Module Active Directory
+Import-Module GroupPolicy
+
+Search-ADAccount -AccountDisabled -UsersOnly | Select-Object Name, SamAccountName
+$timespan = New-TimeSpan -Days 90
+Search-ADAccount -UsersOnly -AccountInactive -TimeSpan $timespan | Disable-ADAccount
+
+Search-ADAccount -LockedOut | Select-Object Name, SamAccountName 
+Unlock-ADAccount -Identity krbtgt
